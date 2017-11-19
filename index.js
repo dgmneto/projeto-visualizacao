7d54e10b09ed81842b1161e6e06f4cead71a0c29
dgmneto <dgmneto@hotmail.com>
2017-11-19 18:02:55 -0300

    chore(index.html): add crossfilter

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ d3.csv('data/data.csv', function(err, data) {
         alert("Some unexpected erro ocurred while trying to download the data");
         return;
     }
-    // habemus datam
+    document.getElementById("loading-status")
+        .innerHTML = "Baixado";
+    
+    data.forEach(element => {
+        element.dt = new Date(element.dt);
+    });
+
+    var ndx = crossfilter(data);
 });
